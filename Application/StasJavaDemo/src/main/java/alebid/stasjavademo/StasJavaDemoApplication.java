59e1a93537935c40071b5eae86dd0f3f984f143f
Andrii Lebid <lebedav@gmail.com>
2024-06-24 21:13:05 -0300

    init java springboot project

---
+++ Application/StasJavaDemo/src/main/java/alebid/stasjavademo/StasJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package alebid.stasjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StasJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StasJavaDemoApplication.class, args);
+	}
+
+}
---
+++ Application/StasJavaDemo/src/test/java/alebid/stasjavademo/StasJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package alebid.stasjavademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StasJavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
