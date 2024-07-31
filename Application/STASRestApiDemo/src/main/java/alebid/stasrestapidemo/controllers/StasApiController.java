cc5f72aa14a41422546fa4396c63c470ddc185ee
Andrii Lebid <lebedav@gmail.com>
2024-07-30 21:02:13 -0300

    add rest api project

---
+++ Application/STASRestApiDemo/src/main/java/alebid/stasrestapidemo/controllers/StasApiController.java
@@ -0,0 +1,8 @@
+package alebid.stasrestapidemo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StasApiController {
+
+}
---
+++ Application/STASRestApiDemo/src/test/java/alebid/stasrestapidemo/StasRestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package alebid.stasrestapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StasRestApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
